abccae74c9bec93eab13f827f329bd46a3fbbb73
Garrison <Garrison@Garrisons-MacBook-Pro.local>
2011-06-03 18:32:23 -0500

    Implement Login validation, error messages, etc

--- Youpon-Pt-2/LoginRootViewController.m
+++ Youpon-Pt-2/LoginRootViewController.m
@@ -53,13 +53,35 @@
 }
 */
 
-/*
+
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad
 {
+    [lblErrorText setText:@""];
+    
+    //Get user defaults to determine additional UI settings
+    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    NSString *hasAuthenticated = [userDefaults objectForKey:@"hasAuthenticated"];
+    NSString *authenticatedUsername = [userDefaults objectForKey:@"authenticatedUsername"];
+    NSString *authenticatedPassword = [userDefaults objectForKey:@"authenticatedPassword"];
+    
+    if ([hasAuthenticated isEqualToString:@"TRUE"]) {
+        [txfUsername setText:authenticatedUsername];
+        [txfPassword setText:authenticatedPassword];
+        
+        [txfUsername setEnabled:FALSE];
+        [txfPassword setEnabled:FALSE];
+    }
+    else if ([hasAuthenticated isEqualToString:@"NOPIN"]) {
+        //Do nothing
+    }
+    else if ([hasAuthenticated isEqualToString:@"FALSE"]) {
+        //Do nothing
+    }
+    
     [super viewDidLoad];
 }
-*/
+
 
 - (void)viewDidUnload
 {
@@ -110,6 +132,9 @@
 }
 
 - (IBAction)loginButtonPressed:(id)sender {
+    [txfUsername resignFirstResponder];
+    [txfPassword resignFirstResponder];
+    [txfPIN resignFirstResponder];
     [self doLoginAction:sender];
 }
 
@@ -121,7 +146,94 @@
 
 #pragma mark - Action Methods
 - doLoginAction:(id)sender {
+    //Reset colors
+    [txfUsername setTextColor:[UIColor blackColor]];
+    [txfPassword setTextColor:[UIColor blackColor]];
+    [txfPIN setTextColor:[UIColor blackColor]];
     
+    //Init ServiceRequest/Response
+    ServiceRequest *loginRequest = [[ServiceRequest alloc] init];
+    ServiceResponse *loginResponse = [[ServiceResponse alloc] init];
+    
+    //Set up Login Request
+    [loginRequest setRequestTime:[NSDate date]];
+    [loginRequest setRequestType:@"LOGIN"];
+    [loginRequest setRequestorUsername:[txfUsername text]];
+    [loginRequest setRequestorPassword:[txfPassword text]];
+    
+    //Get ref to App Delegate
+    Youpon_Pt_2AppDelegate *appDelegate = (Youpon_Pt_2AppDelegate *)[[UIApplication sharedApplication] delegate];
+    
+    //Call service - get service response
+    loginResponse = [appDelegate callLoginService:loginRequest];
+    
+    //Get user defaults to determine flow
+    NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+    NSString *hasAuthenticated = [userDefaults objectForKey:@"hasAuthenticated"];
+    NSString *authenticatedPIN = [userDefaults objectForKey:@"authenticatedPIN"];
+    
+    BOOL goToMainMenu = FALSE;
+
+    if ([hasAuthenticated isEqualToString:@"FALSE"] || [hasAuthenticated isEqualToString:@"NOPIN"] || hasAuthenticated == NULL) {
+        if ([[loginResponse responseCode] isEqualToString:@"SUCCESS"]) {
+            if (![[txfPIN text] isEqualToString:@""]) {
+                [userDefaults setObject:@"TRUE" forKey:@"hasAuthenticated"];
+                [userDefaults setObject:[txfUsername text] forKey:@"authenticatedUsername"];
+                [userDefaults setObject:[txfPassword text] forKey:@"authenticatedPassword"];
+                [userDefaults setObject:[txfPIN text] forKey:@"authenticatedPIN"];
+                goToMainMenu = TRUE;
+            }
+            else {
+                [userDefaults setObject:@"NOPIN" forKey:@"hasAuthenticated"];
+                [userDefaults setObject:[txfUsername text] forKey:@"authenticatedUsername"];
+                [userDefaults setObject:[txfPassword text] forKey:@"authenticatedPassword"];
+                goToMainMenu = TRUE;
+            }
+        }
+        else if ([[loginResponse responseCode] isEqualToString:@"INVALID_USERNAME"]) {
+            [userDefaults setObject:@"FALSE" forKey:@"hasAuthenticated"];
+            
+            [txfUsername setTextColor:[UIColor redColor]];
+            [lblErrorText setText:@"Username is not recognized"];
+            NSLog(@"Invalid login attempt - Invalid username");
+        }
+        else if ([[loginResponse responseCode] isEqualToString:@"INVALID_PASSWORD"]) {
+            [userDefaults setObject:@"FALSE" forKey:@"hasAuthenticated"];
+            
+            [txfPassword setTextColor:[UIColor redColor]];
+            [lblErrorText setText:@"Password does not match"];
+            NSLog(@"Invalid login attempt - Invalid password for this username");
+        }
+    }
+    else if ([hasAuthenticated isEqualToString:@"TRUE"]) {
+        if ([[txfPIN text] isEqualToString:authenticatedPIN]) {
+            [userDefaults setObject:@"TRUE" forKey:@"hasAuthenticated"];
+            
+            goToMainMenu = TRUE;
+        }
+        else {
+            [userDefaults setObject:@"FALSE" forKey:@"hasAuthenticated"];
+            
+            [txfPIN setTextColor:[UIColor redColor]];
+            [lblErrorText setText:@"PIN does not match"];
+            NSLog(@"Invalid login attempt - PIN does not match");
+        }
+    }
+    else {
+        NSLog(@"Invalid login attempt - unrecognized Authentication status: %@", hasAuthenticated);
+    }
+    //Save userdefaults
+    [userDefaults synchronize];
+    
+    [loginRequest release];
+    loginRequest = nil;
+    [loginResponse release];
+    loginResponse = nil;
+    
+    if (goToMainMenu) {
+        [self.parentViewController dismissModalViewControllerAnimated:YES];
+    }
+    return 0;
 }
 
 
