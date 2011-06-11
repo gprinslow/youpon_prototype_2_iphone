53739387687fe3639c8123c743d8051cf7ed1905
Garrison <Garrison@Garrisons-MacBook-Pro.local>
2011-06-10 22:24:00 -0500

    Offer - Ch.4 - Add OffersEditTableViewController (to p.103)

---
+++ Youpon-Pt-2/OffersEditTableViewController.h
@@ -0,0 +1,25 @@
+//
+//  OffersEditTableViewController.h
+//  Youpon-Pt-2
+//
+//  Created by Garrison on 6/10/11.
+//  Copyright 2011 Garrison Prinslow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface OffersEditTableViewController : UITableViewController {
+    NSManagedObject *offer;
+    
+    @private
+    NSArray *sectionNames;
+    NSArray *rowLabels;
+    NSArray *rowKeys;
+    NSArray *rowControllers;
+}
+
+@property (nonatomic, retain) NSManagedObject *offer;
+
+
+@end
---
+++ Youpon-Pt-2/OffersEditTableViewController.m
@@ -0,0 +1,224 @@
+//
+//  OffersEditTableViewController.m
+//  Youpon-Pt-2
+//
+//  Created by Garrison on 6/10/11.
+//  Copyright 2011 Garrison Prinslow. All rights reserved.
+//
+
+#import "OffersEditTableViewController.h"
+#import "NSArray-NestedArrays.h"
+#import "OfferValueDisplay.h"
+
+@implementation OffersEditTableViewController
+
+@synthesize offer;
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    [offer release];
+    [sectionNames release];
+    [rowLabels release];
+    [rowKeys release];
+    [rowControllers release];
+    [super dealloc];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    
+    sectionNames = [[NSArray alloc] initWithObjects:[NSNull null], NSLocalizedString(@"General", @"General"), nil];
+    
+    rowLabels = [[NSArray alloc] initWithObjects:
+                 
+                 //Section 1
+                 [NSArray arrayWithObjects:
+                  NSLocalizedString(@"Title", @"Title"), nil],
+                 
+                 //Section 2
+                 [NSArray arrayWithObjects:
+                  NSLocalizedString(@"Byline", @"Byline"),
+                  NSLocalizedString(@"Category", @"Category"),
+                  NSLocalizedString(@"Number Offered", @"Number Offered"),
+                  NSLocalizedString(@"Start Date", @"Start Date"),
+                  NSLocalizedString(@"Expiration Date", @"Expiration Date"),
+                  nil],
+                 
+                 //Sentinel
+                 nil];
+    
+    rowKeys = [[NSArray alloc] initWithObjects:
+               
+               //Section 1
+               [NSArray arrayWithObjects:@"title", nil],
+               
+               //Section 2
+               [NSArray arrayWithObjects:@"byline", @"category", @"numberOffered", @"startDate", @"expirationDate", nil],
+               
+               //Sentinel
+               nil];
+    
+    //TODO: Populate rowControllers array
+    
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated
+{
+    [super viewDidDisappear:animated];
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    // Return the number of sections.
+    return [sectionNames count];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    // Return the number of rows in the section.
+    return [rowLabels countOfNestedArray:section];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"OfferEditCell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue2 reuseIdentifier:CellIdentifier] autorelease];
+    }
+    
+    // Configure the cell...
+    NSString *rowKey = [rowKeys nestedObjectAtIndexPath:indexPath];
+    NSString *rowLabel = [rowLabels nestedObjectAtIndexPath:indexPath];
+    
+    id <OfferValueDisplay, NSObject> rowValue = [offer valueForKey:rowKey];
+    
+    cell.detailTextLabel.text = [rowValue offerValueDisplay];
+    cell.textLabel.text = rowLabel;
+    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    
+    return cell;
+}
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }   
+    else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Navigation logic may go here. Create and push another view controller.
+    /*
+     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
+     // ...
+     // Pass the selected object to the new view controller.
+     [self.navigationController pushViewController:detailViewController animated:YES];
+     [detailViewController release];
+     */
+    
+    // TODO: Push editing controller onto the stack
+}
+
+#pragma mark - Table view - added methods (Ch. 4)
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    id title = [sectionNames objectAtIndex:section];
+    if ([title isKindOfClass:[NSNull class]]) {
+        return nil;
+    }
+    return title;
+}
+
+@end
